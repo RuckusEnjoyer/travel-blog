c8599323ecdae02d846dcfbc023406658a91a904
Kork <hail3yp@gmail.com>
2023-11-29 18:48:54 -0500

    added tag and comment model

---
+++ models/comment.js
@@ -0,0 +1,52 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+//Compares Login Password to Encrypted Password in db!!
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        blog_id: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            references: {
+                model: 'blog',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment'
+    }
+        
+    
+)
+
+module.exports = Comment;
