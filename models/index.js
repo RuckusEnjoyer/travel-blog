fb2d123a8ded50e74ff467e34ebfd420cf4d066b
caldardn <caldardn@gmail.com>
2023-11-29 19:01:22 -0500

    adding index, blog, and destination code

---
+++ models/index.js
@@ -0,0 +1,40 @@
+const Blog = require('./blog');
+const User = require('./user');
+const Comment = require('./comment');
+const Tag = require('./tag');
+const Destination = require('./destination');
+
+User.hasMany(Blog, {
+    foreignKey: 'user_id',
+   
+})
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    
+})
+
+Blog.belongsTo(User, {
+    foreignKey: 'user_id'
+}) 
+
+Blog.hasMany(Comment, {
+    foreignKey: 'post_id',
+  
+})
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Comment.belongsTo(Blog, {
+    foreignKey: 'post_id'
+})
+Blog.belongsTo(Destination, {
+    foreignKey: 'destination_id'
+})
+
+Tag.hasMany(Blog, {
+    foreignKey: 'tag_id'
+})
+
+module.exports = { User, Blog, Comment, Tag, Destination };
