6ab299b7d50e2fc0b634a1862b82e5dc1b9f7350
Kork <hail3yp@gmail.com>
2023-11-29 19:48:41 -0500

    added code to routes.js | added initial handlebars files

--- controllers/routes.js
+++ controllers/routes.js
@@ -45,7 +45,44 @@ router.get('/dashboard', async (req, res) => {
 })
 //TO DO: GET to an individual Post
 
-//TO DO: GET to the login
+router.get('/blog/:id', async (req, res) => {
+    try{
+        const blogData = await Blog.findByPK({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                },
+                {
+                    model: Tag,
+                    attributes: ['name']
+                },
+                {
+                    model: Comment,
+                    attributes: ['name', 'body', 'user_id']
+                }
+            ]
+        })
+        const blog = blogData.map((blog) => blog.get({ plain: true }));
 
+        res.render('dashboard', {
+            blog,
+            logged_in: req.session.logged_in
+        })
+    } catch (err){
+        console.log(err)
+        res.status(500).json(err)
+    }
+})
+
+//TO DO: GET to the login
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+    res.render('login');
+  });
+  
 //TO DO: logout(?)
 module.exports = router;
