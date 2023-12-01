b707cfe92f62f860ed730706728e3e783330c7ba
Kork <hail3yp@gmail.com>
2023-12-01 12:05:36 -0500

    starting files for map

--- models/user.js
+++ models/user.js
@@ -44,6 +44,10 @@ User.init(
     {
         hooks: {
             //PUT HOOKS HERE IF WE WANT HOOKS
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+              },
         },
         sequelize,
         timestamps: true,
