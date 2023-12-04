340f7d0dd2c3977421f1c6939ecc5bfe31791773
caldardn <caldardn@gmail.com>
2023-12-03 22:38:34 -0500

    adding code for dashboard

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,6 +3,7 @@ const users = require('./usersRoute.js');
 const tags = require('./tagsRoute.js');
 const comments = require('./commentsRoute.js');
 const blogs = require('./blogsRoute.js');
+const dashboard = require('./dashboardRoute.js');
 
 router.use('/blogs', blogs);
 // router.use('/comments', comments);
