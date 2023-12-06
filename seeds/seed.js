4c3fe3b1915957c642dcf81ef05f67a14be7a642
caldardn <caldardn@gmail.com>
2023-12-05 20:31:09 -0500

    adding searching

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,14 +1,14 @@
 const sequelize = require('../config/connection');
 const seedBlog = require('./blogData');
 const seedComment = require('./commentData');
-const seedUser = require('./userData');
+// const seedUser = require('./userData');
 const destination = require('./destinationData');
 const tag = require('./tagData');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
-  await seedUser();
+  // await seedUser();
 
   await destination();
 
