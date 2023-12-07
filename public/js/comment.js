714884ebf3b90dcd2899ea8846f72a34effa65d3
Kork <hail3yp@gmail.com>
2023-12-06 23:56:00 -0500

    playing with comment code and removing unnecessary code from navbar

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,58 +1,99 @@
-// const commentHandler = async (event) => {
-//     event.preventDefault();
+// // const commentHandler = async (event) => {
+// //     event.preventDefault();
   
-//     // const blogElement = document.getElementsByClassName('blog')
-//     // console.log(blogElement)
+// //     // const blogElement = document.getElementsByClassName('blog')
+// //     // console.log(blogElement)
 
-//     const blog_id = event.target.dataset.blogId
-//     console.log(blog_id)
-//     const comment_content = document.querySelector('#comment-content').value.trim();
-//     const user_id = sessionStorage.getItem('user_id')
+// //     const blog_id = event.target.dataset.blogId
+// //     console.log(blog_id)
+// //     const comment_content = document.querySelector('#comment-content').value.trim();
+// //     const user_id = sessionStorage.getItem('user_id')
   
-//     if (comment_content) {
-//       const response = await fetch('/api/comments', {
-//         method: 'POST',
-//         body: JSON.stringify({ blog_id, comment_content, user_id }),
-//         headers: { 'Content-Type': 'application/json' },
-//       });
+// //     if (comment_content) {
+// //       const response = await fetch('/api/comments', {
+// //         method: 'POST',
+// //         body: JSON.stringify({ blog_id, comment_content, user_id }),
+// //         headers: { 'Content-Type': 'application/json' },
+// //       });
   
-//       if (response.ok) {
-//         // document.location.reload();
-//       } else {
-//         alert('Failed to create comment');
-//       }
+// //       if (response.ok) {
+// //         // document.location.reload();
+// //       } else {
+// //         alert('Failed to create comment');
+// //       }
+// //     }
+// //   };
+  
+// //   document
+// //     .querySelector('.new-comment-form')
+// //     .addEventListener('submit', commentHandler);
+
+// const commentHandler = async (event) => {
+//   event.preventDefault();
+
+//   const blogId = event.target.window.parent
+//   console.log(blogId + "this is the blog id")
+//   const comment_content = event.target.querySelector('#comment-content').value.trim();
+//   // const user_id = sessionStorage.getItem('user_id');
+
+//   if (comment_content) {
+//     const response = await fetch(`${window.location.origin}/api/comments`, {
+//       method: 'POST',
+//       body: JSON.stringify({ blog_id: blogId, comment_content }),
+//       headers: { 'Content-Type': 'application/json' },
+//     });
+
+//     if (response.ok) {
+//       // document.location.reload();
+//     } else {
+//       alert('Failed to create comment');
 //     }
-//   };
-  
-//   document
-//     .querySelector('.new-comment-form')
-//     .addEventListener('submit', commentHandler);
+//   }
+// };
+
+// document.addEventListener('submit', (event) => {
+//   if (event.target.classList.contains('.new-comment-form')) {
+//     commentHandler(event);
+//   }
+// });
+
 
 const commentHandler = async (event) => {
   event.preventDefault();
+  try {
+    const content = document.querySelector('#comment-content').value.trim();
 
-  const blogId = event.target.closest('.blog-item').id;
-  console.log(blogId + "this is the blog id")
-  const comment_content = event.target.querySelector('#comment-content').value.trim();
-  const user_id = sessionStorage.getItem('user_id');
+    // Get the form element that the event was triggered on
+    const formElement = event.target;
 
-  if (comment_content) {
-    const response = await fetch('/api/comments', {
-      method: 'POST',
-      body: JSON.stringify({ blog_id: blogId, comment_content, user_id }),
-      headers: { 'Content-Type': 'application/json' },
-    });
+    // Get the parent section element
+    const sectionElement = formElement.parentNode;
 
-    if (response.ok) {
-      // document.location.reload();
-    } else {
-      alert('Failed to create comment');
+    // Get the blog_id from the data-id attribute
+    const blog_id = sectionElement.getAttribute('data-id');
+
+    if (content && blog_id) {
+      const response = await fetch(
+        `/api/comments/`,
+        {
+          method: 'POST',
+          body: JSON.stringify({ content, blog_id }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      if (response.ok) {
+        document.location.reload();
+      }
     }
+  } catch (error) {
+    console.error(error);
   }
 };
 
-document.addEventListener('submit', (event) => {
-  if (event.target.classList.contains('.new-comment-form')) {
-    commentHandler(event);
-  }
-});
+//Event Listeners
+document
+  .querySelector('.new-comment-form')
+  .addEventListener('submit', commentHandler);
